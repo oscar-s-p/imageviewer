98f8a4dd0872cc1397b0c963c6b262239da29fdb
Oscar Soler <osolertf@gmail.com>
2026-01-28 16:04:56 -0800

    bug fix

--- src/imageviewer/image_viewer_class.py
+++ src/imageviewer/image_viewer_class.py
@@ -1273,6 +1273,7 @@ class image_viewer:
                                 if plotting['log']: ax[j].set_yscale('log')
                                 ax[j].legend()
                     ax[0].set_ylabel('Number of observations')
+                    print(np.argwhere(filters_dict.keys()==var))
                     fig.suptitle('Filtering \"%s\" %s %s'%(var, howto_dict[list_filter[np.argwhere(filters_dict.keys()==var)]], filters_dict[var]))
                     plt.tight_layout()
                     plt.show()
