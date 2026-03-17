2bfd6f58612a2ce84e5f039945804d7184e6b91c
Oscar Soler <osolertf@gmail.com>
2026-03-17 13:48:58 -0700

    Bug fix

--- src/imageviewer/__init__.py
+++ src/imageviewer/__init__.py
@@ -5,7 +5,7 @@ A collection of tools for viewing and analyzing astronomical FITS images.
 """
 
 from .image_viewer_class import image_viewer
-from .utils import final_wcs, filter_df, stacking_wcs
+from .utils import final_wcs, filter_df, stacking_wcs, skycoord_from_df, cross_match_radec
 from .photometry import photo_analysis, detect_sources, get_magnitude, get_coordinates, get_catalogue
 from importlib.metadata import version as _version
 
